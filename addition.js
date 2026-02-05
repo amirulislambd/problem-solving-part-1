c4a1cc29262d8975720997e5ad1a28155d7d3030
amirulislambd <amirulislambd313@gmail.com>
2026-02-05 19:18:28 +0600

    addition between tow numbers and addition between tow strin

---
+++ addition.js
@@ -0,0 +1,12 @@
+//simple addition of number
+const number1 = 50;
+const number2 = 501;
+const sum = number1 + number2;
+console.log(sum);
+
+//simple addition of string
+const firstName = "Amirul";
+const lastName = "Islam";
+const fullName = firstName.concat(" ", lastName);
+const fullName2 = firstName + " " + "" + lastName;
+console.log(fullName);
