5e53e90fe5e0867fd3a1a72958a639b2b187ef29
amirulislambd <amirulislambd313@gmail.com>
2026-02-05 19:51:26 +0600

    subtraction

--- addition.js
+++ addition.js
@@ -1,12 +1,14 @@
-//simple addition of number
+//simple addition of numbers
 const number1 = 50;
 const number2 = 501;
-const sum = number1 + number2;
+// const sum = number1 + number2;
+const sum = `total number is: ${number1 + number2} TK`;
 console.log(sum);
 
-//simple addition of string
+//simple addition of strings
 const firstName = "Amirul";
 const lastName = "Islam";
-const fullName = firstName.concat(" ", lastName);
-const fullName2 = firstName + " " + "" + lastName;
+// const fullName = firstName.concat(" ", lastName);
+// const fullName2 = firstName + " " + "" + lastName;
+const fullName = `${firstName} ${lastName}`
 console.log(fullName);
---
+++ subtraction.js
@@ -0,0 +1,7 @@
+//simple subtraction of numbers
+const y = 50;
+// const x = 501;
+const x = "501";
+// const sum = x - y;
+const sum = `total number is: ${x - y} TK`;
+console.log(sum);
